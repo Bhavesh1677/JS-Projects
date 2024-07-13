f4055c89c59395131ccf5e0ca46e335d089bb35a
Bhavesh Pawar <pawarbhavesh16@gmail.com>
2024-07-13 22:05:55 +0530

    Project 6 data fetch from api using XMLHttpRequest 👍

--- Project 6/script.js
+++ Project 6/script.js
@@ -1,4 +1,4 @@
-const requestUrl = 'https://api.github.com/users/hiteshchoudhary';
+const requestUrl = 'https://api.github.com/users/Bhavesh1677';
 const fetchBtn = document.getElementById('fetch-btn');
 const cardContainer = document.getElementById('card-container');
 
@@ -11,12 +11,14 @@ fetchBtn.addEventListener('click', () => {
             const data = JSON.parse(xhr.responseText);
             const avatarUrl = data.avatar_url;
             const name = data.name;
+            const bio = data.bio;
             const followers = data.followers;
 
             const cardHTML = `
                     <div class="card">
                         <img src="${avatarUrl}" alt="Avatar">
                         <h1>${name}</h1>
+                        <p>Bio: ${bio}</p>
                         <p>Followers: ${followers}</p>
                     </div>
                     `;
